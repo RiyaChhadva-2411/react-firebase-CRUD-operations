a7324b4e658e1ada274ed6323918a79717df2ca7
Riya_2411 <riyanchhadva@gmail.com>
2021-03-30 19:01:18 +0530

    add: SecondCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
-import firebase from './Firebase.js';
+import firebase from './Firebase';
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.ref=firebase.firestore().collection('boards');
-    this.unsubscribe= null;
-    this.state={
+    this.ref = firebase.firestore().collection('boards');
+    this.unsubscribe = null;
+    this.state = {
       boards: []
     };
   }
 
-  onCollectUpdate= (querySnapshot)=>{
-    const boards=[];
-    querySnapshot.forEach((doc)=>{
-      const {title, description, author}= doc.data();
+  onCollectionUpdate = (querySnapshot) => {
+    const boards = [];
+    querySnapshot.forEach((doc) => {
+      const { title, description, author } = doc.data();
       boards.push({
         key: doc.id,
-        doc, //DocumentSnapshot
+        doc, // DocumentSnapshot
         title,
         description,
         author,
@@ -27,24 +27,25 @@ class App extends Component{
     });
     this.setState({
       boards
-    });
-  }
-  componentDidMount(){
-    this.unsubscribe= this.ref.onSnapshot(this.onCollectionUpdate);
+   });
   }
 
-  render(){
-    return(
-      <div className="container">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h3 className="panel-title">
-            BOARD LIST
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
+  render() {
+    return (
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              BOARD LIST
             </h3>
           </div>
-          <div className="panel-body">
+          <div class="panel-body">
             <h4><Link to="/create">Add Board</Link></h4>
-            <table className="table table-stripe">
+            <table class="table table-stripe">
               <thead>
                 <tr>
                   <th>Title</th>
@@ -53,9 +54,9 @@ class App extends Component{
                 </tr>
               </thead>
               <tbody>
-                {this.state.boards.map(board=>
+                {this.state.boards.map(board =>
                   <tr>
-                    <td><Link to={'/show/${board.key}'}>{board.title}</Link></td>
+                    <td><Link to={`/show/${board.key}`}>{board.title}</Link></td>
                     <td>{board.description}</td>
                     <td>{board.author}</td>
                   </tr>
@@ -68,4 +69,5 @@ class App extends Component{
     );
   }
 }
+
 export default App;
